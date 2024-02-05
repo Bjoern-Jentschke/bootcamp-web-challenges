b0f3f20160c430a362e76dec47259e2c817a256f
Björn Jentschke <jentschke1@gmail.com>
2024-02-05 10:32:25 +0100

    solution challenge2

---
+++ React-Nesting/react-nesting_fragments/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+import Box from "./components/Box";
+
+export default function App() {
+  return <Boxes />;
+}
+
+function Boxes () {
+  return <>
+      <Box color="#007bff" />
+      <Box color="#fc3" />
+      <Box color="#ff3333" />
+  </>
+}
