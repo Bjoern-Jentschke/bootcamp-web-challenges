f358deb9ca107b63d984c9c62475a9825389f690
Björn Jentschke <jentschke1@gmail.com>
2024-02-06 11:12:06 +0100

    Challenge2

---
+++ React-With-Arrays/react-with-arrays_users/src/components/Tag.js
@@ -0,0 +1,7 @@
+import "./Tag.css";
+
+export default function Tag({ tag }) {
+  console.log(tag);
+  const highlightClass = tag === "admin" ? "tag--highlight" : "";
+  return <li className={"tag" + " " + highlightClass}>{tag}</li>;
+}
