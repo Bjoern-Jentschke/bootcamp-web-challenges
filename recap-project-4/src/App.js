70a7bc66227515403f7a561c18300d7224452e10
Björn Jentschke <jentschke1@gmail.com>
2024-02-09 15:23:07 +0100

    fix App.js

--- recap-project-4/src/App.js
+++ recap-project-4/src/App.js
@@ -1,16 +1,21 @@
-
-import './App.css';
+// import './App.css';
+import Form from './components/Form';
+import {useState} from "react"
+import {uid} from "uid"
 
 function App() {
+   const [activities, setActivities] = useState([]);
+
+   function handleAddActivity(newActivity) {
+      setActivities([{newActivity, id: uid()}, ...activities])
+   }
+
   return (
     <div className="App">
-      <header>
-        <h1>
-        Hello World
-        </h1>
-        </header>
+      <header className="App-header">
+      </header>
+      <Form onAddActivity={handleAddActivity} />
     </div>
   );
 }
-
 export default App;
