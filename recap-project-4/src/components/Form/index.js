29f62bad1c2ee2986a8cdb8cd555184125d54ac1
Björn Jentschke <jentschke1@gmail.com>
2024-02-09 14:51:09 +0100

    Add Activity Form done

--- recap-project-4/src/components/Form/index.js
+++ recap-project-4/src/components/Form/index.js
@@ -1,8 +1,25 @@
 import "./Form.css";
 
-export default function Form() {
+export default function Form({onAddActivity}) {
+
+    function handleSubmit(event) {
+        event.preventDefault();
+     // after preventing the default behaviour of the form element we grab the information of the form elementinto variables
+        const form = event.target;
+        const name = event.target.elements.name.value;
+        const activity = event.target.elements.activity.checked; // checked prevents default behavior (on)
+
+        const newActivity = {name: name, isForGoodWeather: activity} //in newActivity we store the created object
+        console.log(newActivity)
+        onAddActivity(newActivity) //with the call of the onAddActivity prop we can create a new Object with the changed state in the App.js
+        
+        form.reset();
+        form.name.focus();
+
+    }
+
   return (
-    <form name="Activity">
+    <form  onSubmit={handleSubmit} name="Activity">
       <h2>Add new Activity: </h2>
       <div>
         <label htmlFor="name">Name: </label>
@@ -12,7 +29,7 @@ export default function Form() {
         <label htmlFor="activity">Good-weather activity: </label>
         <input id="activity"  type="checkbox" name="activity" aria-label="good weather activity checkbox"></input>
       </div>
-      <button type="onSubmit" aria-label="submit button">Submit</button>
+      <button type="submit" aria-label="submit button">Submit</button>
     </form>
   );
 }
