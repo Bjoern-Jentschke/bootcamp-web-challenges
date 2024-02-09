a8b8a1a24ca8b0020e3840e2390e68b150f99701
Björn Jentschke <jentschke1@gmail.com>
2024-02-09 15:32:25 +0100

    add description for understanding

--- recap-project-4/src/App.js
+++ recap-project-4/src/App.js
@@ -4,9 +4,14 @@ import {useState} from "react"
 import {uid} from "uid"
 
 function App() {
+  // Why setting state in App.js? Possible answer: Parent element of Form.js. Here we want to change the state while
+  // displaying the new activity
    const [activities, setActivities] = useState([]);
 
+   //here we set up the new activity
    function handleAddActivity(newActivity) {
+    //understanding of the following line: the function setActivities is setting the newActivity, passing a unique id to it while creating 
+    // an object and adding with the spread operator the newActivity to the other activities into an array of objects
       setActivities([{newActivity, id: uid()}, ...activities])
    }
 
@@ -14,6 +19,7 @@ function App() {
     <div className="App">
       <header className="App-header">
       </header>
+      {/* passing the function handleAddActivity as a prop to the Form component */}
       <Form onAddActivity={handleAddActivity} />
     </div>
   );
