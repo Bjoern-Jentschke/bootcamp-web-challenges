aa03f327fed75c93ef9fe698ff90200c8cbddc2d
Björn Jentschke <jentschke1@gmail.com>
2024-02-10 12:47:42 +0100

    add solution filtering list

--- Recap-Project/recap-project-4/src/App.js
+++ Recap-Project/recap-project-4/src/App.js
@@ -20,15 +20,61 @@ function App() {
       setActivities([{...newActivity, id: uid()}, ...activities])
    }
 
+  //setting const isGoodWeather and also for isBadWeather for bad weather
    const isGoodWeather = true;
+   const isBadWeather = false;
+
+  //filtering the activities for good weather + training different function writings
+
+  //anonymous function with implicit return
+  // const filteredGoodWeatherActivities = activities.filter((activity) => activity.isForGoodWeather === isGoodWeather)
+
+  // anonymous function with explicit return
+
+  // const filteredGoodWeatherActivities = activities.filter((activity) => {
+  //   return activity.isForGoodWeather === isGoodWeather;
+  // })
+
+  // anonymous function with explicit return and if/else statement
+
+  // const filteredGoodWeatherActivities = activities.filter((activity) => {
+  //   if (activity.isForGoodWeather === isGoodWeather) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // })
+
+  //named function with filter method and if/else statement and explicit return (important: adding the isGoodWeather const  argument, also down in the list element (line 76))
+
+  function filteredGoodWeatherActivities (isGoodWeather) {
+  return activities.filter((activity) => {
+    if (activity.isForGoodWeather === isGoodWeather) {
+      return true;
+    } else {
+      return false;
+    }
+  })}
+
+  const filteredBadWeatherActivities = activities.filter((activity) => activity.isForGoodWeather === isBadWeather)
 
   return (
     <div className="App">
       <header className="App-header">
       </header>
       {/* passing the function handleAddActivity as a prop to the Form component */}
-      <List activities={activities}/>
       <Form onAddActivity={handleAddActivity} />
+
+      <h3>Bad Weather Activity</h3>
+      {/* Add a list element for good weather as well as for bad weather:
+      the activities which we defined in our state and filtered in GoodWeatherActivities as well as BadWeatherActivities are going to be displayed here separately*/}
+      <List 
+      activities={filteredBadWeatherActivities}
+            isGoodWeather={isBadWeather}
+      />
+      <h3>Good Weather Activity</h3>
+      <List activities={filteredGoodWeatherActivities(isGoodWeather)}
+            isGoodWeather = {isGoodWeather}/>
     </div>
   );
 }
